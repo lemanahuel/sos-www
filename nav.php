9e4cee537637e6ce73e6ecf65330ac9419cfedc2
Juan Pablo <solarijuanpablo>
2016-12-20 20:50:07 -0300

    IMPROVE Add logo

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
       <div class="row">
         <div class="col-md-12">
           <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#fh5co-navbar" aria-expanded="false" aria-controls="navbar"><span>Menu</span> <i></i></a>
-          <a href="index.html" class="navbar-brand"><span>voluntariosos</span></a>
+          <a href="index.html" class="navbar-brand"><span><img src="images/logo.png" alt="" class="img-responsive" style="display: inline; margin-right: 5px;">voluntariosos</span></a>
         </div>
       </div>
     </div>
